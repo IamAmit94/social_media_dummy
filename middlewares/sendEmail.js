e3da759a361d872889e4334ec12b08e87606daaf
unknown <amitrvt18@hotmail.com>
2024-01-15 08:26:29 +0530

    initial commit

---
+++ middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodemailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a92002d070685d",
+      pass: "01630f9e31e954"
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
